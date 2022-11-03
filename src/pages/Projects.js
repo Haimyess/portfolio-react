3ad6e5f067a686d157de6ea85afaa86ddcfd9d59
Haim Yeshurun <haimyess@gmail.com>
2022-11-03 12:41:04 +0200

    Fixed projects links

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,16 +16,16 @@ function Projects() {
     {
       id: 1,
       name: "Shopy Ecommerce Fullstack",
-      img: "https://live.staticflickr.com/65535/52472171153_10148b4eef_o.jpg",
+      img: "https://live.staticflickr.com/65535/52471628481_42bb155c82_o.jpg",
       link: "https://shopy-ecommerce-app.netlify.app/",
       github: "https://github.com/Haimyess/ecommerce-fullstack-frontend",
     },
     {
       id: 2,
       name: "100 Days Coding React",
-      img: "https://live.staticflickr.com/65535/52471628481_42bb155c82_o.jpg",
+      img: "https://live.staticflickr.com/65535/52472171153_10148b4eef_o.jpg",
       link: "https://100daycoding.netlify.app/",
-      github: "https://github.com/Haimyess/ecommerce-fullstack-frontend",
+      github: "https://github.com/Haimyess/100daysofcode",
     },
   ];
 
